0d922afab44824ce38ac6f9d10e19fbf9ed73838
FGRX <fabien.grignoux@outlook.com>
2020-07-10 12:18:05 +0200

    add lazyloading to helloworld component

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,7 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
@@ -41,7 +37,8 @@
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
+const HelloWorld = () => import("@/components/HelloWorld.vue");
 
 export default Vue.extend({
   name: "App",
